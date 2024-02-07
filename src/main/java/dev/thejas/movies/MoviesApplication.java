7f7db8fba5b0f0d3e4bb38808dd4951156b45d40
Thejas0604 <meddepolamact.21@uom.lk>
2024-02-07 11:48:59 +0530

    movie class

--- src/main/java/dev/thejas/movies/MoviesApplication.java
+++ src/main/java/dev/thejas/movies/MoviesApplication.java
@@ -2,13 +2,15 @@ package dev.thejas.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
+
 }
-//comment test 4
